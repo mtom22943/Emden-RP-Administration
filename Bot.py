e673189ae2d07475a20ab8edd90f98aecc53b470
mtom22943 <mtom22943@gmail.com>
2026-06-11 15:04:53 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -59,12 +59,8 @@ async def on_member_join(member: discord.Member):
 
     if channel:
         embed = discord.Embed(
-            title="👋 Willkommen aufn den Bau Server!",
+            title="👋 Willkommen auf den Bau Server!",
             description=f"Willkommen {member.mention} auf dem Server!
-
-📜 Bitte beachte die Regeln und behandle alle Spieler respektvoll.
-
-🏗️ Viel Spaß auf den Server! ✨",
             color=discord.Color.green()
         )
         await channel.send(embed=embed)
